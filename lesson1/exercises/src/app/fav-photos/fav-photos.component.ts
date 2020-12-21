6005d6c4668b5fe74695f136e82fd25e3cbac023
Desiree Dixon <desireedixon@Desirees-MacBook-Pro.local>
2020-12-20 19:39:54 -0600

    Saving images and text changes for Part 1 of exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://d2a2wjuuf1c30f.cloudfront.net/product_photos/29089691/murakami-blog_original.jpg';
+  image3 = 'https://media.newyorker.com/photos/5f6299960fc96ad919ad25e7/master/pass/200928_r37098.jpg';
 
   constructor() { }
 
