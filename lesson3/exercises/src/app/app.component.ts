7479a6bba449ee5056247fbd70a668e06aec1e33
Desiree Dixon <desireedixon@Desirees-MacBook-Pro.local>
2021-01-07 19:06:29 -0600

    commit

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,62 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+       this.takeOffEnabled = false;
+    }
+ }
+  handleLand(rocketImage){
+    window.alert("The shuttle is landing. Landing gear engaged.")
+    this.color = 'green';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'The shuttle has landed.'
+    rocketImage.style.bottom = '0px';
+    this.takeOffEnabled = true;
+  }
+  handleAbort(rocketImage){
+    let result = window.confirm('Are you sure you want to abort the mission?')
+    if (result) {
+      this.message = "Mission aborted.";
+      this.color = "red";
+      this.height = 0;
+      this.width = 0;
+      rocketImage.style.bottom = '0px';
+      this.takeOffEnabled = true;
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;// I'm confused about what adding to the width does??
+    }
+    if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.width = this.width + 10000;
+    }
+    if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.width = this.width + 10000; 
+    }
+ }
+
 }
+
